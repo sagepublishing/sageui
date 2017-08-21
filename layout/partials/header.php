a6fd3d2e15f074a867d915bc8432c5b3720618c7
atrnoble <atrnoble@gmail.com>
2017-08-21 14:02:23 +0100

    teachers needing additional role

--- layout/partials/header.php
+++ layout/partials/header.php
@@ -73,10 +73,8 @@ if (isloggedin() && !isguestuser()) {
     $preferences = new moodle_url('/user/preferences.php');
     $switchroleurl ='';
     $siteurl=$_SERVER['REQUEST_URI'];
-    if(strpos($siteurl,'course/view.php')){
     $roleid = $DB->get_field('role', 'id', ['shortname' => 'editingteacher']);
 $isteacheranywhere = $DB->record_exists('role_assignments', ['userid' => $USER->id, 'roleid' => $roleid]);
-    }
     
       if(is_siteadmin()||$isteacheranywhere) {
         $switchroleurl = new moodle_url('/course/switchrole.php', array('id' => 1,'switchrole' => -1,'returnurl' => '/my/index.php'));
